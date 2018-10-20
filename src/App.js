55726758598c547103340004dd5b4a20510f8bcb
Bill <skeche@gmail.com>
2018-10-20 16:25:38 -0400

    changes to map

--- src/App.js
+++ src/App.js
@@ -112,10 +112,6 @@ class App extends Component {
             {this.state.pageOne.map((campaign) => {
               return <div>
                   <h2>{campaign.title}</h2>
-                  <h2>{campaign.title}</h2>
-                  <h2>{campaign.title}</h2>
-                  <img src={campaign.image} />
-                  <img src={campaign.image} />
                   <img src={campaign.image} />
                 </div>;
               })
